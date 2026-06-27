e5192efc2492d74a01d905620f1232eadb9a737c
Rajendra Chaudhari <rajendra.m.chaudhari2001@gmail.com>
2026-06-27 17:32:58 +0530

    Add First hello agent implementation in index.js
    - Introduced a new file index.js that defines a Hello Agent using OpenAI's agent framework.
    - The agent responds differently based on the specified location, providing a personalized greeting.
    - Integrated dotenv for environment variable management and set up the agent to run with a sample input.

---
+++ index.js
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import { Agent, run } from '@openai/agents';
+
+const location = "india"
+
+const helloAgent = new Agent({
+    name: "Hello Agent",
+    instructions:  function () {
+        if (location === 'india') {
+          return 'Always say namaste and then You are an agent that always says hello world with users name';
+        } else {
+          return 'That just talk to the user';
+        }
+      },
+    model: 'gpt-4o-mini',
+});
+
+
+run(helloAgent, 'Hey There, My name is Rajendra Chaudhari').then((result) => {
+    console.log(result.finalOutput);
+  });
